05b2d0a631762395f2b5fdb7a2a477e2909538b7
Serge Y. Stroobandt <1105595+stroobandt@users.noreply.github.com>
2023-07-28 19:51:05 +0200

    configuration files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'APRS 434'
+copyright = '2022-2023, CC BY 4.0, Serge Y. Stroobandt, ON4AA'
+author = 'Serge Y. Stroobandt, ON4AA'
 
-release = '0.1'
-version = '0.1.0'
+release = '0.5'
+version = '2023.07'
 
 # -- General configuration
 
@@ -17,6 +17,11 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    'myst_parser',
+]
+
+myst_enable_extensions = [
+    'colon_fence',
 ]
 
 intersphinx_mapping = {
@@ -28,7 +33,6 @@ intersphinx_disabled_domains = ['std']
 templates_path = ['_templates']
 
 # -- Options for HTML output
-
 html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
